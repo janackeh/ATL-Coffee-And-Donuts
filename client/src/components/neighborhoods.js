95b35d05e02683cef9a4d327442e809b2245ac40
Janackeh Blackwell <janackeh@Janackehs-MacBook-Air.local>
2019-08-14 13:18:18 -0400

    created neighborhoods component

---
+++ client/src/components/neighborhoods.js
@@ -0,0 +1,48 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+
+/* Step 2
+ * Rename this class to reflect the component being created
+ *
+ */
+export default class Neighborhoods extends Component {
+
+    /* Step 3
+    * Create a state for the component to store view data
+    *
+    */
+    state = {
+        neighborhoods: [],
+        isNewFormDisplayed: false,
+        addNewNeighborhood: [],
+        newNeighborhood: "",
+    };
+
+    /* Step 4
+    * Use componentDidMount to retrieve any data to display
+    *   Here you can make calls to your local express server
+    *   or to an external API
+    *   setState can be run here as well
+    *   -REMINDER remember `setState` it is an async function
+    */
+        componentDidMount() {
+        this.getAllEvents();
+        }
+
+    /* Step 5
+    *  The render function manages what is shown in the browser
+    *  TODO: delete the jsx returned
+    *   and replace it with your own custom jsx template
+    *
+    */
+    render() {
+        return (
+            <div>
+                {/* Accessing the value of message from the state object */}
+                <h1>{this.state.message}</h1>
+            </div>
+        )
+    }
+}
