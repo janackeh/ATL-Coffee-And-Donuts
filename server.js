0a636fff86539a2c5c7d6a505eb5af2ad3e1eec3
Janackeh Blackwell <janackeh@Janackehs-MacBook-Air.local>
2019-08-14 11:37:46 -0400

    updated server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const app = express()
  * import routers from controllers/
  *
  */
-const { templateRouter } = require('./controllers/template.js')
+const { neighborhoodRouter } = require('./controllers/neighborhoods.js')
+const { coffeeRouter } = require('./controllers/coffee.js')
+const { donutRouter } = require('./controllers/donuts.js')
 
 
 /* Step 3
@@ -49,7 +51,9 @@ app.use(express.static(`${__dirname}/client/build`))
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
-app.use('/api/helloworld', templateRouter)
+app.use('/api/neighborhoods', neighborhoodRouter)
+app.use('/api/coffee', coffeeRouter)
+app.use('/api/donuts', donutRouter)
 
 /* Step 5
  *
