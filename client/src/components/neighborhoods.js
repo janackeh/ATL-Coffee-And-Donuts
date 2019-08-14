4f292da6ab88717cb630c0d7fa16945053923416
Janackeh Blackwell <janackeh@Janackehs-MacBook-Air.local>
2019-08-14 13:27:46 -0400

    completed neighborhood component

--- client/src/components/neighborhoods.js
+++ client/src/components/neighborhoods.js
@@ -28,9 +28,38 @@ export default class Neighborhoods extends Component {
     *   -REMINDER remember `setState` it is an async function
     */
         componentDidMount() {
-        this.getAllEvents();
+        this.getAllNeighborhoods();
         }
 
+        getAllNeighborhoods = () => {
+            axios.get("/api/neighborhoods/").then(res => {
+              console.log(res.data);
+              this.setState({ neighborhoods: res.data });
+            });
+          };
+        
+          handleToggleNewForm = () => {
+            this.setState(state => {
+              return { isNewFormDisplayed: !state.isNewFormDisplayed };
+            });
+          };
+        
+          handleInputChange = neighborhoods => {
+            const copiedNeighborhood = { ...this.state.newNeighborhood };
+            copiedNeighborhood[event.target.name] = event.target.value;
+        
+            this.setState({ newNeighborhood: copiedNeighborhood });
+          };
+        
+          handleSubmit = neighborhoods => {
+            event.preventDefault();
+        
+            axios.post("/api/neighborhoods/", this.state.newNeighborhood).then(res => {
+              this.setState({ isNewFormDisplayed: false });
+              this.getAllNeighborhoods();
+            });
+          };
+        
     /* Step 5
     *  The render function manages what is shown in the browser
     *  TODO: delete the jsx returned
@@ -38,11 +67,51 @@ export default class Neighborhoods extends Component {
     *
     */
     render() {
-        return (
-            <div>
-                {/* Accessing the value of message from the state object */}
-                <h1>{this.state.message}</h1>
-            </div>
-        )
-    }
-}
+        let neighborhoodsList = this.state.neighborhoods.map(neighborhood => {
+            return (
+              <div>
+      
+                <Link key={neighborhood._id} to={`/neighborhoods/${neighborhood._id}`}>
+                  {neighborhood.name}
+                </Link>
+              </div>
+            );
+          });
+          return this.state.isNewFormDisplayed ? (
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="new-neighborhood-name">Neighborhood Name</label>
+              <input
+                type="text"
+                id="new-neighborhood-name"
+                name="name"
+                onChange={this.handleInputChange}
+                value={this.state.newNeighborhood.name}
+              />
+      
+              <label htmlFor="new-neighborhood-location">Neighborhood Location</label>
+              <input
+                type="text"
+                id="new-neighborhood-location"
+                name="location"
+                onChange={this.handleInputChange}
+                value={this.state.newEvent.location}
+              />
+      
+              <input type="submit" value="Create Neighborhood" />
+            </form>
+          ) : (
+              <div>
+                <img src="https://i.imgur.com/7EVpbk2.png" alt="logo"></img>
+                <h1> Neighborhoods </h1>
+                {neighborhoodsList}
+                <div>
+                  <button onClick={this.handleToggleNewForm}>Create New Neighborhood</button>
+                </div>
+              </div>
+            );
+        }
+      }
+    
+      
+    
+
