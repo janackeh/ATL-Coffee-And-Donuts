155dc255881f21261248119803be70bdbd9c0ab1
Janackeh Blackwell <janackeh@Janackehs-MacBook-Air.local>
2019-08-19 13:21:24 -0400

    added new route paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Neighborhoods from "./components/neighborhoods.jsx";
+import Neighborhood from "./components/neighborhood.jsx";
 import Coffees from "./components/coffees.jsx";
+import Coffee from "./components/coffee.jsx";
 import './App.css';
 
 function App() {
@@ -10,7 +12,10 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/neighborhoods" component={Neighborhoods}/>
+          <Route path="/neighborhood" component={Neighborhood}/>
           <Route path="/coffees" component={Coffees}/>
+          <Route path="/coffee" component={Coffee}/>
+        
         </Switch>
       </Router>
     </div>
