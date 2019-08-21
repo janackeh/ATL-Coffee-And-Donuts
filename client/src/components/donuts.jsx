39ce767195d194c1cccc166fd19b795da27b3b47
Janackeh Blackwell <janackeh@Janackehs-MacBook-Air.local>
2019-08-20 20:56:53 -0400

    started donuts component

---
+++ client/src/components/donuts.jsx
@@ -0,0 +1,39 @@
+/* Step 1 import React, { Component } and axios
+ *
+ */
+import React, { Component } from 'react'
+import axios from 'axios'
+
+/* Step 2
+ * Rename this class to reflect the component being created
+ *
+ */
+export default class Donuts extends Component {
+
+    state = {
+        message: ''
+    }
+
+
+    componentDidMount() {
+        axios.get('/api/helloworld')
+            .then((res) => {
+                this.setState({message: res.data})
+            })
+    }
+
+    /* Step 5
+    *  The render function manages what is shown in the browser
+    *  TODO: delete the jsx returned
+    *   and replace it with your own custom jsx template
+    *
+    */
+    render() {
+        return (
+            <div>
+                {/* Accessing the value of message from the state object */}
+                <h1>{this.state.message}</h1>
+            </div>
+        )
+    }
+}
