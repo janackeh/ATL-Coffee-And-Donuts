f4591fac8c628604630a19cac2707706d3f130de
Janackeh Blackwell <janackeh@Janackehs-MacBook-Air.local>
2019-08-20 21:46:10 -0400

    started donut component

---
+++ client/src/components/donut.jsx
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class SingleDonut extends Component {
+
+    state = {
+        message: ''
+    }
+
+    componentDidMount() {
+        axios.get('/api/helloworld')
+            .then((res) => {
+                this.setState({message: res.data})
+            })
+    }
+    
+    render() {
+        return (
+            <div>
+                {/* Accessing the value of message from the state object */}
+                <h1>{this.state.message}</h1>
+            </div>
+        )
+    }
+}
